382e5c61a98ecf6715f15a762dbe46e990d3d78c
eryczou <eryczou@gmail.com>
2017-10-12 21:10:04 -0400

    add react hot loader module

---
+++ app/components/hello.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './hello.less'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className="hello-component">
+                Hello, React, why?what?
+            </div>
+        );
+    }
+}
+
+export default Hello;
